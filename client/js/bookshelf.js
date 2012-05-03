05398e2277862be637bf158a2c6b511681425d93
roshihy <mita3itatoto@gmail.com>
2012-05-04 03:44:48 +0900

    タグ付け機能の実装

--- client/js/bookshelf.js
+++ client/js/bookshelf.js
@@ -1,46 +1,130 @@
+
+//////////////////////////////////////////////
+//コレクション定義
+//
 Books = new Meteor.Collection("books");
 
-Template.book_list.books = function () {
-	  return Books.find({}, {sort: {_id: -1}});
+//////////////////////////////////////////////
+//セッション定義
+//
+Session.set('tag_filter', null);
+Session.set('editing_addtag', null);
+Session.set('editing_itemname', null);
+
+//////////////////////////////////////////////
+//Handlebars変数定義
+//
+
+//書籍の貸し出し状態を判別して返す
+Template.book.borrow = function () {
+	return this.borrow ? "alert" : "";
 };
 
-Template.book_list.events = {
+//タグフィルター用のタグ名/カウント数を返す
+Template.tag_filter.tags = function () {
+  var tag_infos = [];
+  var total_count = 0;
+
+  Books.find({}).forEach(function (book) {
+    _.each(book.tags, function (tag) {
+      var tag_info = _.find(tag_infos, function (x) { return x.tag === tag; });
+      if (! tag_info)
+        tag_infos.push({tag: tag, count: 1});
+      else
+        tag_info.count++;
+    });
+    total_count++;
+  });
+
+  tag_infos = _.sortBy(tag_infos, function (x) { return x.tag; });
+  tag_infos.unshift({tag: null, count: total_count});
+
+  return tag_infos;
+};
+
+//タグのテキスト、なければ「すべて選択」を返す
+Template.tag_item.tag_text = function () {
+  return this.tag || "すべて選択";
+};
+
+//選択されているタグの識別にために"selected"文言を返す
+Template.tag_item.selected = function () {
+  return Session.equals('tag_filter', this.tag) ? 'selected' : '';
+};
+
+//各書籍に付与されているタグを配列にして返す
+Template.book.tags_list = function () {
+	var book_id = this._id;
+	return _.map(this.tags || [], function (tag) {
+		return {book_id: book_id, tag: tag};
+	});
+};
+
+//adding_tagの真偽を返す（タグ追加編集に入れば真）
+Template.book.adding_tag = function () {
+	return Session.equals('editing_addtag', this._id);
+};
+
+//選択されているタグにより、表出する書籍を変更する
+Template.book_list.books = function () {
+	  var tag_filter = Session.get('tag_filter');
+	  var sel = {};
+	  if (tag_filter){
+		  sel.tags = tag_filter;
+	  }
+	  return Books.find(sel, {sort: {borrow: false, titleKana: 1}});
+};
+
+
+//////////////////////////////////////////////
+//イベント定義
+//
+Template.book.events = {
 	'click a.borrow' : function () {
-		var isbn = $(arguments[0].target).next().val();
-	    Books.update({isbn: isbn}, {$set: {borrow: true}});
+	    Books.update({_id: this._id}, {$set: {borrow: true}});
 	},
 	'click a.return' : function () {
-		var isbn = $(arguments[0].target).next().val();
-	    Books.update({isbn: isbn}, {$set: {borrow: false}});
-	}
+	    Books.update({_id: this._id}, {$set: {borrow: false}});
+	},
+	'click .addtag': function (evt) {
+		Session.set('editing_addtag', this._id);
+		Meteor.flush();
+		focus_field_by_id("edittag-input");
+	},
 };
 
 Template.modal_add.events = {
 	'click a.add' : function(){
-
 		var isbn = $("input#isbn-input").val();
-
 		//バリデーション
 		if(isbn.match(/[^0-9]+/)){
 			alert("ISBN-13として不正な値です：「" + isbn + "」");
 			return;
-		};
-
-		if(!isbn.length === 13){
+		} else if(!isbn.length === 13){
 			alert("桁数が不正です：「" + isbn + "」");
 			return;
-		};
-
-		if(Books.find({isbn: isbn}).count() !=　0){
+		} else if(Books.find({isbn: isbn}).count() !=　0){
 			alert("すでにその書籍は登録されています。");
 			return;
 		};
-
-		var bookInfo = {};
-		bookInfo = BS.rakutenApiCall(isbn);
+		//書籍情報を取得しDBに格納する処理
+		BS.rakutenApiCall(isbn);
 	}
 };
 
+Template.tag_item.events = {
+	'mousedown': function () {
+		if (Session.equals('tag_filter', this.tag)){
+			Session.set('tag_filter', null);
+		} else {
+			Session.set('tag_filter', this.tag);
+		}
+	}
+};
+
+	//////////////////////////////////////////////
+//関数定義
+//
 var BS = {};
 (function(){
 	BS.rakutenUrl = 'http://api.rakuten.co.jp/rws/3.0/json';
@@ -71,6 +155,7 @@ var BS = {};
 						image : item["mediumImageUrl"],
 						itemUrl : item["itemUrl"],
 						price: item["itemPrice"],
+						titleKana: item["titleKana"],
 						isbn : isbn,
 					};
 					Books.insert(ret);
@@ -85,3 +170,11 @@ var BS = {};
 		});
 	};
 })();
+
+var focus_field_by_id = function (id) {
+  var input = document.getElementById(id);
+  if (input) {
+    input.focus();
+    input.select();
+  }
+};
